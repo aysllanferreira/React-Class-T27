17c466815a997d22ae1c7784b5a89e404228f902
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-12-05 14:02:13 -0300

    Resolucao feita

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,103 @@ import Card from './components/Card';
 import Inputs from './components/Inputs';
 
 class App extends Component {
+  state = {
+    nome: '',
+    alcunha: '',
+    imagem: '',
+    descricao: '',
+    savedCards: [],
+  };
+
+  componentDidMount() {
+    const pegarLS = JSON.parse(localStorage.getItem('cardSalvo'));
+
+    if (pegarLS !== null) {
+      this.setState({
+        savedCards: pegarLS,
+      });
+    }
+  }
+
+  saveLocalStorage = () => {
+    const { savedCards } = this.state;
+    localStorage.setItem('cardSalvo', JSON.stringify(savedCards));
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = () => {
+    const {
+      nome, alcunha, imagem, descricao, savedCards,
+    } = this.state;
+
+    this.setState({
+      savedCards: [
+        ...savedCards,
+        {
+          nome,
+          alcunha,
+          imagem,
+          descricao,
+        },
+      ],
+      nome: '',
+      alcunha: '',
+      imagem: '',
+      descricao: '',
+    }, this.saveLocalStorage);
+  };
+
+  removeItem = ({ target }) => {
+    const getName = target.id;
+    const { savedCards } = this.state;
+
+    const verifyDeleted = savedCards.filter((cards) => cards.nome !== getName);
+    this.setState({
+      savedCards: verifyDeleted,
+    }, this.saveLocalStorage);
+  };
+
   render() {
+    const {
+      nome, alcunha, imagem, descricao, savedCards,
+    } = this.state;
     return (
       <div className="App">
         <div className="inputs">
-          <Inputs />
-          <Card />
+          <Inputs
+            nome={nome}
+            alcunha={alcunha}
+            imagem={imagem}
+            descricao={descricao}
+            handleChange={this.handleChange}
+            handleClick={this.handleClick}
+          />
+          <Card
+            nome={nome}
+            alcunha={alcunha}
+            imagem={imagem}
+            descricao={descricao}
+          />
         </div>
-        <div className="Cards" />
+        <div className="Cards">
+          {savedCards.length > 0 && savedCards.map((card) => (
+            <div key={card.nome}>
+              <Card
+                nome={card.nome}
+                alcunha={card.alcunha}
+                imagem={card.imagem}
+                descricao={card.descricao}
+              />
+              <button onClick={this.removeItem} id={card.nome} type="button">Remover</button>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import './Card.scss';
 
 class Card extends Component {
   render() {
+    const {
+      nome, alcunha, imagem, descricao,
+    } = this.props;
     return (
       <div className="card">
-        <h1>nome</h1>
-        <h2>alcunha</h2>
+        <h1>{nome}</h1>
+        <h2>{alcunha}</h2>
         <img
-          src="https://sm.ign.com/ign_br/screenshot/default/blob_pgw1.jpg"
-          alt="Imagem: $NOME"
+          src={imagem}
+          alt={`Imagem: ${nome}`}
         />
-        <p>descricao</p>
+        <p>{descricao}</p>
       </div>
     );
   }
 }
+
+Card.propTypes = {
+  nome: PropTypes.string,
+  alcunha: PropTypes.string,
+  imagem: PropTypes.string,
+  descricao: PropTypes.string,
+}.isRequired;
 
 export default Card;
